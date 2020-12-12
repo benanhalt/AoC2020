a73fa2aa3f3292b42cccc920667d6cc6299c1357
Ben Anhalt <ben.anhalt@gmail.com>
2020-12-12 13:01:41 -0600

    improvements to day 11.

--- day11/solution.hs
+++ day11/solution.hs
@@ -1,78 +1,72 @@
 {-# LANGUAGE NamedFieldPuns, ScopedTypeVariables #-}
 
-import Data.List (tails, scanl', delete, sort)
+import Data.List (tails, foldl', delete, sort)
 import Control.Monad (guard)
 import Debug.Trace (traceShowId, traceShow)
 import Data.Function (fix)
+
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+
+type Grid = Map (Int, Int) Char
 
 main :: IO ()
 main = do
   input <- lines <$> readFile("input.txt")
 
+  let grid = Map.fromList $ do
+        (r, line) :: (Int, [Char]) <- zip [0..] input
+        (c, p) :: (Int, Char) <- zip [0..] line
+        pure $ ((r,c), p)
+
   putStrLn "Part 1:"
-  let end = findRepeat step input
-  putStrLn $ show $ length $ filter (== '#') $ concat end
+  let end = findRepeat step grid
+  putStrLn $ show $ Map.foldl' (\a p -> if p == '#' then a + 1 else a) 0 end
 
   putStrLn "Part 2:"
-  let end = findRepeat step' input
-  putStrLn $ show $ length $ filter (== '#') $ concat end
+  let end = findRepeat step' grid
+  putStrLn $ show $ Map.foldl' (\a p -> if p == '#' then a + 1 else a) 0 end
 
 findRepeat :: Eq a => (a -> a) -> a -> a
-findRepeat f a = if f a == a then a else findRepeat f $ f a
+findRepeat f a = let fa = f a in if fa == a then a else findRepeat f fa
 
 -- If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
 -- If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
 -- Otherwise, the seat's state does not change.
 
-countAd :: [[Char]] -> (Int, Int) -> Int
-countAd s (r, c) = sum $ do
+countAd :: Grid -> (Int, Int) -> Int
+countAd grid (r, c) = foldl' (+) 0 $ do
   dr <- [-1, 0, 1]
   dc <- [-1, 0, 1]
   guard $ (dr, dc) /= (0, 0)
   let (r', c') = (r + dr, c + dc)
-  guard $ r' >= 0 && r' < length s
-  guard $ c' >= 0 && c' < length (s !! r)
-  pure $ if (s !! r') !! c' == '#' then 1 else 0
+  pure $ if Map.lookup (r', c') grid == Just '#' then 1 else 0
 
-step :: [[Char]] -> [[Char]]
-step s = do
-  r <- [0..length s - 1]
-  pure $ do
-    c <- [0..length (s !! r) - 1]
-    let p = (s !! r) !! c
-    pure $ case p of
-      'L' -> if countAd s (r, c) == 0 then '#' else p
-      '#' -> if countAd s (r, c) > 3 then 'L' else p
+
+step :: Grid -> Grid
+step grid = Map.mapWithKey rule grid
+  where
+    rule (r,c) p = case p of
+      'L' -> if countAd grid (r, c) == 0 then '#' else p
+      '#' -> if countAd grid (r, c) > 3 then 'L' else p
       _ -> p
 
-justCount :: [[Char]] -> [[Char]]
-justCount s = do
-  r <- [0..length s - 1]
-  pure $ do
-    c <- [0..length (s !! r) - 1]
-    let p = (s !! r) !! c
-    pure $ case p of
-      'L' -> if countAd s (r, c) == 0 then '#' else p
-      '#' -> head $ show $ countAd s (r, c)
-      _ -> p
 
-countSee :: [[Char]] -> (Int, Int) -> Int
-countSee s (r, c) = sum $ do
+countSee :: Grid -> (Int, Int) -> Int
+countSee grid (r, c) = sum $ do
   dr <- [-1, 0, 1]
   dc <- [-1, 0, 1]
   guard $ (dr, dc) /= (0, 0)
-  pure $ (\c -> if c == '#' then 1 else 0) $ head $ dropWhile (== '.') $ do
+  pure $ (\c -> if c == Just '#' then 1 else 0) $ head $ dropWhile (== Just '.') $ do
     m <- [1..]
     let (r', c') = (r + m * dr, c + m * dc)
-    pure $ if r' < 0 || c' < 0 || r' >= length s || c' >= length (s !! r') then 'E' else (s !! r') !! c'
+    pure $ Map.lookup (r', c') grid
 
-step' :: [[Char]] -> [[Char]]
-step' s = do
-  r <- [0..length s - 1]
-  pure $ do
-    c <- [0..length (s !! r) - 1]
-    let p = (s !! r) !! c
-    pure $ case p of
-      'L' -> if countSee s (r, c) == 0 then '#' else p
-      '#' -> if countSee s (r, c) > 4 then 'L' else p
+step' :: Grid -> Grid
+step' grid = Map.mapWithKey rule grid
+  where
+    rule (r,c) p = case p of
+      'L' -> if countSee grid (r, c) == 0 then '#' else p
+      '#' -> if countSee grid (r, c) > 4 then 'L' else p
       _ -> p
+
